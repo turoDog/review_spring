f5ebb1a4f3c72bbdbe31b029019716b2d3250a94
chenzhiyuan <turodog@foxmail.com>
2019-09-01 09:36:50 +0800

    :tada: SpringMVC 拦截器

---
+++ mvc/mvc_interceptor/src/main/java/com/nasus/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.nasus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @RequestMapping("/testInterceptor")
+    public String testInterceptor(){
+        System.out.println("testInterceptor执行了...");
+        return "success";
+    }
+
+}
---
+++ mvc/mvc_interceptor/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/5/5
+  Time: 22:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h3>拦截器</h3>
+
+<a href="user/testInterceptor" >拦截器</a>
+
+</body>
+</html>
